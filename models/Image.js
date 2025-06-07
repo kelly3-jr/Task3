a44889dc4f60d2a53237571b2b8a3a2c49408659
kelly3-jr <a.kellyjosephineraj@gmail.com>
2025-06-07 13:17:30 +0530

    Update Image.js

--- models/Image.js
+++ models/Image.js
@@ -3,10 +3,22 @@
 const mongoose = require('mongoose');
 
 const imageSchema = new mongoose.Schema({
-  filename: { type: String, required: true },
-  mimetype: { type: String, required: true },
-  size: { type: Number, required: true },
-  uploadedAt: { type: Date, default: Date.now },
+  filename: { 
+    type: String, 
+    required: true 
+  },
+  mimetype: {
+    type: String, 
+    required: true
+  },
+  size: { 
+    type: Number, 
+    required: true
+  },
+  uploadedAt: { 
+    type: Date,
+    default: Date.now
+  },
 });
 
 module.exports = mongoose.model('Image', imageSchema);
