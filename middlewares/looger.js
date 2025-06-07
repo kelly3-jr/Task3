3f4a06de425d9ee84a6b1d6fd2501cd2005cd45b
kelly3-jr <a.kellyjosephineraj@gmail.com>
2025-06-07 13:15:05 +0530

    logger.js

--- middlewares/looger.js
+++ middlewares/looger.js
@@ -1,5 +1,5 @@
-// ✅ Q3. Middleware to log method and URL
-// ✅ Q7. Append request logs to access.log
+//  Q3. Middleware to log method and URL
+//  Q7. Append request logs to access.log
 
 const fs = require('fs');
 const path = require('path');
